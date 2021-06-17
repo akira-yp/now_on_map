7bf16540b5e31ca87c40ad32c752d530a120716b
akira-yp <yog.puripuri@gmail.com>
2021-06-17 22:51:34 +0900

    OAuthを使用しないアカウント登録機能を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users,controllers:{
-    registrations: "users/registrations"
+    registrations: "users/registrations",
+    sessions: 'users/sessions',
+    omniauth_callbacks: "users/omniauth_callbacks"
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
