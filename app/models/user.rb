f4ed48e06ee7a45e9f82e01d04a773e564c32fbb
akira-yp <yog.puripuri@gmail.com>
2021-06-19 13:05:19 +0900

    userとeventにvalidationを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, { presence: true,
+                    format: { with: VALID_EMAIL_REGEX}}
 
   has_many :events
   # Include default devise modules. Others available are:
