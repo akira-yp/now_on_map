44f84363df220fa39bb398bb79f7829e5d66260e
akira-yp <yog.puripuri@gmail.com>
2021-07-03 00:45:44 +0900

    開始終了日時のバリデーションとデフォルト日時を設定

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,15 @@
 class Event < ApplicationRecord
-  validates :title, presence: true
-  validates :description, presence: true
-  validates :start_date, presence: true
-  validates :end_date, presence: true
-  validates :location, presence: true
-  validates :latitude, presence: true
-  validates :longitude, presence: true
+  with_options presence: true do
+    validates :title
+    validates :description
+    validates :start_date
+    validates :end_date
+    validates :location
+    validates :latitude
+    validates :longitude
+  end
+
+  validate :start_end_check
 
   belongs_to :user
   has_many :categorizings, dependent: :destroy
@@ -17,6 +21,10 @@ class Event < ApplicationRecord
   has_many :hashtags, through: :hashtagings, source: :hashtag
 
   mount_uploader :image, ImageUploader
+
+  def start_end_check
+    errors.add(:end_date, "は開始日より後になるよう設定してください") if self.start_date > self.end_date
+  end
 
   after_create do
     event = Event.find_by(id: self.id)
