67ba12b898e89fb3963902f3426eabb1e220a513
akira-yp <yog.puripuri@gmail.com>
2021-07-08 23:57:22 +0900

    （修正）マイページのルーティングとアクション名を変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  def show
+  def mypage
+    @events = current_user.favorite_events.includes(:categories).all
+    gon.events = @events.map { |event | {
+      'event': event,
+      'categories': event.categories.pluck(:name),
+      'date': "#{event.start_date.strftime("%Y年%m月%d日")} ~ #{event.end_date.strftime("%Y年%m月%d日")}"
+      }
+    }
+  end
+
+  def posts_index
     @events = current_user.events.includes(:categories).all
     gon.events = @events.map { |event | {
       'event': event,
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,11 @@ Rails.application.routes.draw do
     post 'users/guest_sign_in', to:'users/sessions#guest_sign_in'
     post 'users/guest_admin_sign_in', to:'users/sessions#guest_admin_sign_in'
   end
-  resources :users, only: [:show]
+  resources :users do
+    member do
+      get :mypage
+    end
+  end
   resources :events do
     collection do
       get '/hashtag/:name', to:'events#hashtag'
