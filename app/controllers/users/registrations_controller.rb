3ab26fff10d881ae05e3454163cd5a07001b16e4
akira-yp <yog.puripuri@gmail.com>
2021-07-16 23:06:07 +0900

    googleログインした人のパスワード変更とemailを変更できないように処理

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,4 +10,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys:[:name,:image,:image_cache])
   end
+
+  def update_resource(resource, params)
+    resource.update_without_current_password(params)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -49,4 +49,17 @@ class User < ApplicationRecord
     user.save
     user
   end
+
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
 end
