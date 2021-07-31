b13a6ed75b295409f6bd11c60220fc057a331c4d
akira-yp <yog.puripuri@gmail.com>
2021-07-31 10:54:42 +0900

    管理者機能を含むユーザー機能のシステムスペックを作成

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -17,6 +17,7 @@ RailsAdmin.config do |config|
   ## == PaperTrail ==
   # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
 
+  # config.parent_controller = 'ApplicationController' 
   ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
 
   ## == Gravatar integration ==
--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,57 +1,51 @@
 require 'rails_helper'
 
-RSpec.describe 'イベント投稿機能', type: :system do
-  before do
-    FactoryBot.create(:category)
-    @user = FactoryBot.create(:user)
-    visit new_user_session_path
-    fill_in 'メールアドレス', with:'user1@test.com'
-    fill_in 'パスワード', with:'password'
-    click_button 'ログイン'
-  end
-
-  describe 'イベント新規投稿機能' do
+RSpec.describe 'ユーザー機能', type: :system do
+  describe 'アカウント登録' do
     before do
-      visit events_path
-      page.first('#mapid').click
-      click_link 'この場所にイベントを投稿'
-      fill_in 'イベント名', with:'テストイベント'
-      fill_in '開催地', with:'テスト市テスト町一丁目'
-      fill_in 'イベント詳細', with:'テストイベントの詳細 #ハッシュタグテスト'
-      find('label', text: 'category1', match: :first ).click
-      click_on '投稿'
+      visit new_user_session_path
+      click_link 'アカウント登録'
+      fill_in '名前', with:'testuser'
+      fill_in 'メールアドレス', with:'testuser@test.com'
+      fill_in 'パスワード', with:'password'
+      fill_in 'パスワード（確認用）', with:'password'
+      click_button 'アカウント登録'
     end
 
-    context '新規作成した場合' do
-      it '地図上にイベントが表示される',js: true do
-        expect(page).to have_selector '.leaflet-marker-icon'
-      end
-    end
-
-    context 'ハッシュタグを表記した場合' do
-      it 'ハッシュタグにリンクが表示される', js: true do
-        find('.leaflet-marker-icon').click
-        click_link 'テストイベント'
-        expect(page).to have_link '#ハッシュタグテスト'
-      end
-    end
-
-  end
-
-  describe 'イベント検索機能' do
-    it 'デフォルト状態で現在開催中のイベントが表示される' do
-
-    end
-    context 'カテゴリーで検索した場合' do
-      it '一致したカテゴリーを含むイベントが表示される' do
-
-      end
-    end
-    context '開催日で検索した場合' do
-      it '開催期間内に開催日があるイベントが表示される' do
-
-      end
+    it 'ログアウトボタンが表示される' do
+      expect(page).to have_selector 'a', text:'ログアウト'
     end
   end
+  describe '管理ページ機能' do
+    let!(:admin_user){ FactoryBot.create(:admin_user)}
+    let!(:normal_user){ FactoryBot.create(:user2)}
 
+    context '管理者がアクセスした場合' do
+      before do
+        visit new_user_session_path
+        fill_in 'メールアドレス', with:'admin_user@test.com'
+        fill_in 'パスワード', with:'password'
+        click_button 'ログイン'
+      end
+      it '管理者ページに遷移できる' do
+        visit rails_admin_path
+        expect(page).to have_selector 'h1',text:'サイト管理'
+      end
+    end
+
+    context '通常ユーザーがアクセスした場合' do
+      before do
+        visit new_user_session_path
+        fill_in 'メールアドレス', with:'user2@test.com'
+        fill_in 'パスワード', with:'password'
+        click_button 'ログイン'
+      end
+      it 'エラーメッセージが表示される' do
+        visit rails_admin_path
+        expect(page).to have_selector 'p',text:'アクセス権限がありません'
+      end
+    end
+
+
+  end
 end
