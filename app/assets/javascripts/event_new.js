23b67ab3acf7181a338168b9006766f1aad7499c
akira-yp <yog.puripuri@gmail.com>
2021-08-03 12:54:49 +0900

    地図タイルにmaptilerを導入

--- app/assets/javascripts/event_new.js
+++ app/assets/javascripts/event_new.js
@@ -6,6 +6,8 @@ var popup = L.popup();
 function onMapClick(e) {
   mymap.flyTo([e.latlng.lat,e.latlng.lng], 18, { duration: 1 });
   popup.setLatLng(e.latlng).setContent(`<a data-remote="true" href="/events/new?latitude=${e.latlng.lat.toFixed(6)}&longitude=${e.latlng.lng.toFixed(6)}">この場所にイベントを投稿</a>`).openOn(mymap);
+  console.log(e.latlng.lat.toFixed(6));
+  console.log(e.latlng.lng.toFixed(6));
 }
 
 mymap.on('click', onMapClick);
--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -50,6 +50,8 @@ var popup = L.popup();
 function onMapClick(e) {
   mymap.flyTo([e.latlng.lat,e.latlng.lng], 16, { duration: 1 });
   popup.setLatLng(e.latlng).setContent(`<div><a data-remote="true" href="/events/new?latitude=${e.latlng.lat.toFixed(6)}&longitude=${e.latlng.lng.toFixed(6)}">この場所にイベントを投稿</a></div><div><a data-remote="true" href="/users/${gon.user_id}/mylocations/new?my_latitude=${e.latlng.lat.toFixed(6)}&my_longitude=${e.latlng.lng.toFixed(6)}">この場所にマイロケーションを作成</div>`).openOn(mymap);
+  // console.log(e.latlng.lat.toFixed(6));
+  // console.log(e.latlng.lng.toFixed(6));
 }
 
 mymap.on('click', onMapClick);
