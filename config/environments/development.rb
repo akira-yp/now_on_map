33366c78e55e4b5ff925bb74e30b05ded3506707
akira-yp <yog.puripuri@gmail.com>
2021-08-08 15:45:18 +0900

    (修正)Uglifierの設定を追記し、JSのアセットエラーを修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,6 +49,8 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  config.assets.js_compressor = Uglifier.new(harmony: true)
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
