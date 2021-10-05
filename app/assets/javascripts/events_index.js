d511fa4ada6081170187647dd0fffb9b4c8ed361
akira-yp <yog.puripuri@gmail.com>
2021-10-05 16:48:29 +0900

    現在地表示をボタンクリック時に指定

--- app/assets/javascripts/events_index.js
+++ app/assets/javascripts/events_index.js
@@ -14,15 +14,6 @@ L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/
   minZoom: 3,
 }).addTo(mymap);
 
-// 現在地取得
-// function onLocationFound(e) {
-//     mymap.setView(e.latlng);
-// }
-
-// function onLocationError(e) {
-//     console.log("現在地を取得できませんでした。" + e.message);
-// }
-
 //markerClusterを設定
 var markers = L.markerClusterGroup();
 
